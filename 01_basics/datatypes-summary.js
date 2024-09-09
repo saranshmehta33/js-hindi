310a66a7a7c0f141f9aa17865e9400e2e9b62fe2
Saransh Mehta <101050311+saranshmehta33@users.noreply.github.com>
2024-09-09 17:15:14 +0000

    stack and heap memory

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -33,3 +33,31 @@ const myFunction = function(){
 }
 
 console.log(typeof heros);
+
+
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubename = "saranshmehta33"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "saransh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
