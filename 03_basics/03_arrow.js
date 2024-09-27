028023a0d4b9be4601d146a604500a53bd7b2ad9
Saransh Mehta <101050311+saranshmehta33@users.noreply.github.com>
2024-09-27 17:35:31 +0000

    scope this and arrow

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,54 @@
+const user = {
+    username: "saransh",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "saransh"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "saransh"
+//     console.log(this.username);
+// }
+
+const chai =  () => {
+    let username = "saransh"
+    console.log(this);
+}
+
+// chai()
+
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) =>  ( num1 + num2 )         // implicit return
+
+const addTwo = (num1, num2) =>  ({username: "saransh"})         
+
+
+console.log(addTwo(3, 4))
+
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
